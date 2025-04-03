3faf9ff3bdf115648c29031e7a1220a1a5c6fe4b
bhoomi2716 <bhoomiwadhvana@gmail.com>
2025-04-03 22:03:59 +0530

    commit

--- Testing in Android/app/src/main/java/com/example/assignment_7_2/RegistrationUtil.kt
+++ Testing in Android/app/src/main/java/com/example/assignment_7_2/RegistrationUtil.kt
@@ -2,27 +2,29 @@ package com.example.assignment_7_2
 
 class RegistrationUtil
 {
-    private val existingUsers = listOf("abc")
+    private val existingUsers = listOf("Rohan")
 
-    fun validRegistrationInput(
-        userName : String,
+    fun validRegistrationInput(userName : String,
         password : String,
-        confirmPassword : String
-    ) : Boolean
-    {
+        confirmPassword : String) : Boolean {
+
         if (userName.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()){
+
             return false
         }
 
         if (userName in existingUsers){
+
             return false
         }
 
         if (password != confirmPassword){
+
             return false
         }
 
         if (password.count { it.isDigit() } < 2){
+
             return false
         }
         return true
